07d55175c2f085d6578e2173f3479504ecff2875
Issei Ikeda <issei.ikeda@zozo.com>
2023-01-16 18:27:20 +0900

    不要なコードを追加

--- app/src/main/java/com/example/qodana_sample/MainActivity.kt
+++ app/src/main/java/com/example/qodana_sample/MainActivity.kt
@@ -2,9 +2,10 @@ package com.example.qodana_sample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
-    val unneccesarry: Int = 0
+    val unneccesarry: Int? = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
