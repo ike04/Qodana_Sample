611f88dfc310b89fd4b06b0d67239566be991160
Issei Ikeda <issei.ikeda@zozo.com>
2023-02-03 10:47:47 +0900

    Add warning

--- app/src/main/java/com/example/qodana_sample/MainActivity.kt
+++ app/src/main/java/com/example/qodana_sample/MainActivity.kt
@@ -1,17 +1,16 @@
 package com.example.qodana_sample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-    val unneccesarry: Int? = 0
+    val unneccesarry2: Int? = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
-    fun test(s: String?): Int? = s?.let { it.length }
+    fun test2(s: String?): Int? = s?.let { it.length }
 
     fun main() {
         listOf(1, 2, 3).filter { it > 1 }.count()
