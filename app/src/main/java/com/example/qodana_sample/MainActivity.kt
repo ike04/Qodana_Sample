e006c1b75a1c577007303183967bb85ab8672d9b
Issei Ikeda <issei.ikeda@zozo.com>
2023-02-03 18:34:34 +0900

    Remove unneccesary code

--- app/src/main/java/com/example/qodana_sample/MainActivity.kt
+++ app/src/main/java/com/example/qodana_sample/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-    val unneccesarry2: Int? = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
